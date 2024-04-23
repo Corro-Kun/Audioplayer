a0b2b986b2ac263a5f5747db31ca570cf0873638
Corro kun <networpava@gmail.com>
2024-04-23 02:34:06 -0500

    update version

--- src/components/BarBottonMain/BarBottonMain.tsx
+++ src/components/BarBottonMain/BarBottonMain.tsx
@@ -1,14 +1,20 @@
 import Made from "../Made/Made"
 import {useNavigate} from "react-router-dom";
 import "./BarBottonMain.css"
+import {getVersion} from "@tauri-apps/api/app";
+import { useEffect, useState } from "react";
 
 function BarBottonMain(){
     const navigate = useNavigate();
+    const [version, setVersion] = useState("");
+    useEffect(()=>{
+        getVersion().then((data)=>setVersion(data));
+    },[]);
     return(
         <div className="BarBottonMain-Render" >
             <div className="BarBottonMain-Bar" >
                 <div className="BarBottonMain-Bar-Vercion" >
-                    <h2>V0.3.5</h2>
+                    <h2>V{version}</h2>
                 </div>
                 <div className="BarBottonMain-Bar-button" >
                     <button onClick={()=> navigate("/home")} >Entrar</button>
