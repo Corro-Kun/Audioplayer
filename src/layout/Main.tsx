eb05349229f679acc4583ea098db25b393a2006f
Corro kun <networpava@gmail.com>
2024-04-23 19:43:17 -0500

    config updater

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,8 +1,32 @@
+import { checkUpdate, installUpdate, onUpdaterEvent } from "@tauri-apps/api/updater";
 import BarBottonMain from "../components/BarBottonMain/BarBottonMain";
 import BarTopMain from "../components/BarTopMain/BarTopMain";
 import BodyWallpaper from "../components/BodyWallpapar/BodyWallpaper";
+import {useEffect} from "react";
+import { toast } from "sonner";
+import { relaunch } from "@tauri-apps/api/process";
 
 function Main(){
+
+    async function update(){
+        await onUpdaterEvent(({error, status})=>{
+            console.log("no mames",error,status);
+        });
+        try{
+            const {shouldUpdate, manifest} = await checkUpdate();
+            if(!shouldUpdate) return;
+            toast.loading(`Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`)
+            await installUpdate();
+            await relaunch();
+        }catch(e){
+            toast.error("Error al actualizar "+e);
+        }
+    }
+
+    useEffect(()=>{
+        update();
+    },[]);
+
     return(
         <BodyWallpaper>
             <BarTopMain />
