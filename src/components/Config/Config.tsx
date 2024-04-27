357e5ea613e69d67e67affcfebfbed1fdfd732d3
Corro kun <networpava@gmail.com>
2024-04-26 20:25:29 -0500

    create a new range

--- src/components/Config/Config.tsx
+++ src/components/Config/Config.tsx
@@ -20,33 +20,54 @@ function Config(){
                 <div className="List-Title" onClick={ChangerConfigState}  >
                     <h2>Configuraciones</h2>
                 </div>
-                <div className="Checkbox-div" >
-                    <label>{CloseAnimationV? "Activar": "Desactivar"} fondos</label>
-                    <input type="checkbox" ref={InputVideo} onChange={ChangerStateListFondos} />
+                <div className="List-Config-Render" >
+                    <div className="Checkbox-div" >
+                        <label>{CloseAnimationV? "Activar": "Desactivar"} fondos</label>
+                        <input type="checkbox" ref={InputVideo} onChange={ChangerStateListFondos} />
+                    </div>
+                    <div className="Checkbox-div" >
+                        <label>{CloseAnimation? "Activar": "Desactivar"} lista</label>
+                        <input type="checkbox" ref={InputMusic} onChange={ChangerStateListMusic} />
+                    </div>
+                    <div className="Checkbox-div" >
+                        <label>Color de letras</label>
+                        <input onChange={(e)=> ChangerColorLabel(e)} ref={InputColorLabel} style={{display:"none"}} type="color" value={document.documentElement.style.getPropertyValue("--Text_Color")} />
+                        <button onClick={()=> InputColorLabel.current.click()} style={{backgroundColor:"var(--Text_Color)"}} className="Checkbox-div-Color" ></button>
+                    </div>
+                    <div className="Checkbox-div" >
+                        <label>Color de bordes</label>
+                        <input onChange={(e)=> ChangerColorBorder(e)} ref={InputColorBorder} style={{display:"none"}} type="color" />
+                        <button onClick={()=> InputColorBorder.current.click()} style={{backgroundColor:"var(--Border_Color)"}} className="Checkbox-div-Color" ></button>
+                    </div>
+                    <div className="Checkbox-div" >
+                        <label>Color de sombras</label>
+                        <input onChange={(e)=> ChangerColorShadow(e)} ref={InputColorShadow} style={{display:"none"}} type="color" />
+                        <button onClick={()=> InputColorShadow.current.click()} style={{backgroundColor:"var(--Shadow_Color)"}} className="Checkbox-div-Color" ></button>
+                    </div>
+                    <div className="Config-Ranger" >
+                        <div>
+                            <label>Opacidad</label>
+                            <label>100</label>
+                        </div>
+                        <input type="range" min="0" max="30" onChange={(e)=>{
+                            let progress = Number(e.target.value) / Number(e.target.max) * 100;
+                            e.target.style.background = `linear-gradient(to right, var(--Text_Color) 0%, var(--Text_Color) ${progress}%, var(--Border_Color) ${progress}%, var(--Border_Color) 100%)`;
+                        }} />
+                    </div>
+                    <div className="Config-Ranger" >
+                        <div>
+                            <label>Densidad de sombras</label>
+                            <label>10</label>
+                        </div>
+                        <input type="range" min="0" max="10" onChange={(e)=>{
+                            let progress = Number(e.target.value) / Number(e.target.max) * 100;
+                            e.target.style.background = `linear-gradient(to right, var(--Text_Color) 0%, var(--Text_Color) ${progress}%, var(--Border_Color) ${progress}%, var(--Border_Color) 100%)`;
+                        }} />
+                    </div>
+                    <div style={save? {}: {display: "none"}} className="List-Config-Button" >
+                        <button onClick={()=> Save()} >Guardar</button>
+                    </div>
                 </div>
-                <div className="Checkbox-div" >
-                    <label>{CloseAnimation? "Activar": "Desactivar"} lista</label>
-                    <input type="checkbox" ref={InputMusic} onChange={ChangerStateListMusic} />
-                </div>
-                <div className="Checkbox-div" >
-                    <label>Color de letras</label>
-                    <input onChange={(e)=> ChangerColorLabel(e)} ref={InputColorLabel} style={{display:"none"}} type="color" value={document.documentElement.style.getPropertyValue("--Text_Color")} />
-                    <button onClick={()=> InputColorLabel.current.click()} style={{backgroundColor:"var(--Text_Color)"}} className="Checkbox-div-Color" ></button>
-                </div>
-                <div className="Checkbox-div" >
-                    <label>Color de bordes</label>
-                    <input onChange={(e)=> ChangerColorBorder(e)} ref={InputColorBorder} style={{display:"none"}} type="color" />
-                    <button onClick={()=> InputColorBorder.current.click()} style={{backgroundColor:"var(--Border_Color)"}} className="Checkbox-div-Color" ></button>
-                </div>
-                <div className="Checkbox-div" >
-                    <label>Color de sombras</label>
-                    <input onChange={(e)=> ChangerColorShadow(e)} ref={InputColorShadow} style={{display:"none"}} type="color" />
-                    <button onClick={()=> InputColorShadow.current.click()} style={{backgroundColor:"var(--Shadow_Color)"}} className="Checkbox-div-Color" ></button>
-                </div>
-                <div style={save? {}: {display: "none"}} className="List-Config-Button" >
-                    <button onClick={()=> Save()} >Guardar</button>
-                </div>
- 
             </div>
             <div className="List-Div-Config-Button" style={configState? {} : {display: "none"}} >
                 <h2 onClick={ChangerConfigState} ><GoPencil/></h2>
