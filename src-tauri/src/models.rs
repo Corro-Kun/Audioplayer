e0f978f1902f518f4456b091efa65428ce98206d
Corro kun <networpava@gmail.com>
2024-04-30 18:56:31 -0500

    changed color default

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -24,9 +24,9 @@ impl Color{
     pub fn default() -> Vec<Color>{
         vec![
             Color::new(1, "--Text_Color".to_string(), "#fff".to_string()),
-            Color::new(2, "--Border_Color".to_string(), "#000".to_string()),
-            Color::new(3, "--Shadow_Color".to_string(), "#000000".to_string()),
-            Color::new(4, "--Background_Color".to_string(), "#198eee".to_string()),
+            Color::new(2, "--Border_Color".to_string(), "#f48623".to_string()),
+            Color::new(3, "--Shadow_Color".to_string(), "#ee7c11".to_string()),
+            Color::new(4, "--Background_Color".to_string(), "#f29945".to_string()),
         ]
     }
 }
@@ -49,9 +49,9 @@ impl Config{
 
     pub fn default() -> Vec<Config>{
         vec![
-            Config::new(1, "--Blur_Px".to_string(), "15px".to_string()),
-            Config::new(2, "--Shadow_Px".to_string(), "20px".to_string()),
-            Config::new(3, "--Border_Radio_Px_Primary".to_string(), "20px".to_string())
+            Config::new(1, "--Blur_Px".to_string(), "0px".to_string()),
+            Config::new(2, "--Shadow_Px".to_string(), "15px".to_string()),
+            Config::new(3, "--Border_Radio_Px_Primary".to_string(), "15px".to_string())
         ]
     }
 }
